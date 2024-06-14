c99a226ca5844289ad4b01af53858131b4b488d4
Andreas Kwong <andreaskwong@gmail.com>
2024-06-14 11:21:56 +0100

    Finished C.R.D

--- Firefly/ContentView.swift
+++ Firefly/ContentView.swift
@@ -8,14 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var newTodo: String = ""
+    @State private var todos: [Todo] = []
+    private let firebaseManager = FirebaseManager.shared
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                if todos.isEmpty {
+                    Text("Add your first todo below")
+                }
+                else {
+                    ForEach(todos, id:\.id) { todo in
+                        VStack(alignment: .leading) {
+                            Text(todo.content)
+                                .font(.title3)
+                                .fontWeight(.light)
+                            Text(todo.createdAt.formatted(date: .abbreviated, time: .shortened))
+                                .foregroundStyle(.gray)
+                                .font(.caption)
+                        }
+                    }
+                    .onDelete(perform: { indexSet in
+                        indexSet.forEach { index in
+                            firebaseManager.deleteTodo(id: todos[index].id) {error in
+                                if let error = error {
+                                    print("Error: \(error.localizedDescription)")
+                                }
+                            }
+                        }
+                        todos.remove(atOffsets: indexSet)
+                    })
+                }
+            }
+            .listStyle(.plain)
+            
+            Divider()
+            
+            TextField("Enter a todo:", text: $newTodo)
+                .onSubmit {
+                    if newTodo.count > 0 {
+                        firebaseManager.saveTodo(todo: newTodo)
+                        newTodo = ""
+                        
+                        firebaseManager.getTodos { todos, error in
+                            if let error = error {
+                                print("Error: \(error.localizedDescription)")
+                            }
+                            else {
+                                guard let todos = todos else {
+                                    print("Sonething's gone wrong")
+                                    return
+                                }
+                                
+                                self.todos = todos.sorted {
+                                    $0.createdAt < $1.createdAt
+                                }
+                            }
+                        }
+                    }
+                }
+                .padding()
+                .navigationTitle("Firefly")
         }
-        .padding()
+        .onAppear {
+            firebaseManager.getTodos { todos, error in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                }
+                else {
+                    guard let todos = todos else {
+                        print("Sonething's gone wrong")
+                        return
+                    }
+                    
+                    self.todos = todos.sorted {
+                        $0.createdAt < $1.createdAt
+                    }
+                }
+            }
+        }
     }
 }
 
--- Firefly/Models/Todo.swift
+++ Firefly/Models/Todo.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+struct Todo {
+    let id: String
+    let content: String
+    let createdAt: Date
+    
+    init(id: String, data: [String: Any]) {
+        self.id = id
+        self.content = data["content"] as? String ?? "My todo"
+        let timestamp = data["createdAt"] as? Timestamp ?? nil
+        self.createdAt = timestamp?.dateValue() ?? Date()
+    }
+  
+    init(id: String, content: String, createdAt: Date) {
+        self.id = id
+        self.content = content
+        self.createdAt = createdAt
+    }
+}
